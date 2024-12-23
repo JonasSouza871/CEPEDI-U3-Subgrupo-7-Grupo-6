a820ada078d80867ffc11e659b7e8c7cb56b1103
Jonas Souza <jonasssouza871@hotmai.com>
2024-12-23 10:41:41 -0300

    Ler a unidade de destino

--- Conversao de Volume.c
+++ Conversao de Volume.c
@@ -7,5 +7,13 @@ int main() {
     printf("Digite o valor: ");
     scanf("%f", &valor);
 
+    printf("Escolha a unidade de origem:\n");
+    printf("1 - Litro\n2 - Mililitro\n3 - Metro cubico\n");
+    scanf("%d", &origem);
+
+    printf("Escolha a unidade de destino:\n");
+    printf("1 - Litro\n2 - Mililitro\n3 - Metro cubico\n");
+    scanf("%d", &destino);
+
     return 0;
 }
